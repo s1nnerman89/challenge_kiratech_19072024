a6dcbda73476bf24754dcc0da0e22e36bed13e91
s1nnerman89 <sinnerman89@hotmail.it>
2024-07-27 17:56:14 +0200

    disabled outputs

--- phase4-provision_kubernetes_cluster/terraform/outputs.tf
+++ phase4-provision_kubernetes_cluster/terraform/outputs.tf
@@ -1,6 +1,6 @@
 # THESE ARE OUTPUTTED BY rke AND FED TO kubernetes
 
-output "kube_config" {
-  value = rke_cluster.kiratech_rancher_cluster.kube_config_yaml
-  sensitive = true
-}
+#output "kube_config" {
+  #value = rke_cluster.kiratech_rancher_cluster.kube_config_yaml
+  #sensitive = true
+#}
